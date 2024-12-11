0fbef849020f74a7b87698729b259635614abdb1
Jabi <michal.jabi@gmail.com>
2024-12-11 16:06:37 +0100

    explain[Header.tsx]: passing props and destructuring.

--- auction-portal/src/App.tsx
+++ auction-portal/src/App.tsx
@@ -3,9 +3,11 @@ import {Header} from "./Header.tsx";
 
 function App() {
 
-    console.log('App działa!')
+  //  console.log('App działa!')
   return (
     <>
+        <Header myLuckyNumber={879} />
+        <Header title="Jakiś inny tytuł" />
         <Header />
     </>
   )
--- auction-portal/src/Header.tsx
+++ auction-portal/src/Header.tsx
@@ -1,7 +1,32 @@
-export function Header() {
+
+interface Props {
+    title?: string,
+    myLuckyNumber?: number
+}
+
+// export function Header(props: {title?: string}) {
+export function Header({ title = 'Auction Portal', myLuckyNumber }: Props) {
+
     return (
         <header className="mb-2 p-5 bg-warning">
-            <h1>Auction portal</h1>
+            <h1>{title}</h1>
+            <h4>{myLuckyNumber}</h4>
         </header>
     )
 }
+
+
+
+
+/*
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function HeaderOtherExample(props: Props) {
+    // Destukturyzacja dopiero tutaj:
+    const { title = 'Auction Portal' } = props;
+    return (
+        <header className="mb-2 p-5 bg-warning">
+            <h1>{title}</h1>
+        </header>
+    )
+}
+*/
