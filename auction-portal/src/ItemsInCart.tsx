7342526f35132ec9ac8e1b7f1d2ef65fc63950df
Jabi <michal.jabi@gmail.com>
2024-12-12 10:54:49 +0100

    explain: conditional rendering without touching JSX.

--- auction-portal/src/ItemsInCart.tsx
+++ auction-portal/src/ItemsInCart.tsx
@@ -7,14 +7,24 @@ interface Props {
 export function ItemsInCart({counter}: Props) {
 
     // if(counter === 0 || counter === undefined){
-    if(!counter){
-        return "";
-    }
+   /* if (!counter) {
+        return <div className="alert alert-primary" role="alert">
+            Nie masz elementów w koszyku, postaraj się coś wybrać
+        </div>;
+    }*/
 
+    const computedText = !counter ?
+        'Nie masz elementów w koszyku, postaraj się coś wybrać'
+        :
+        `Masz już ${counter} elementów w koszyku`
 
-    return (
+    return (<div className="alert alert-primary" role="alert">
+        {computedText}
+    </div>)
+
+    /*return (
         <div className="alert alert-primary" role="alert">
             Masz już {counter} elementów w koszyku
         </div>
-    );
+    );*/
 }
