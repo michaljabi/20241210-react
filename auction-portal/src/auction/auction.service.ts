f05c2203179c7d1875c3f98fcc149b225918fd5a
Jabi <michal.jabi@gmail.com>
2024-12-13 11:36:27 +0100

    BREAKING: install axios + feat: auction.service

---
+++ auction-portal/src/auction/auction.service.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import {AuctionItem} from "./AuctionItem.ts";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3000',
+})
+
+const BASE_ENDPOINT = '/auctions'
+
+// Mój stateless REST service do zapytań AJAX
+export const auctionService = {
+    async getAll() {
+        return axiosInstance.get(BASE_ENDPOINT);
+    },
+    // mapped Types in TS
+    async getOne(id: AuctionItem['id']) {
+       // console.log(id);
+        return axiosInstance.get(BASE_ENDPOINT+'/'+id);
+    },
+    // utility Types in TS
+    async addOne(auction: Omit<AuctionItem, 'id'>) {
+        //console.log(auction);
+        return axiosInstance.post(BASE_ENDPOINT, auction);
+    }
+}
