334bfb3d53805b31bc0f3b6783771d5f4275487d
Jabi <michal.jabi@gmail.com>
2024-12-13 11:38:35 +0100

    refactor(auction.service): typing AuctionItem.

--- auction-portal/src/auction/auction.service.ts
+++ auction-portal/src/auction/auction.service.ts
@@ -10,16 +10,16 @@ const BASE_ENDPOINT = '/auctions'
 // Mój stateless REST service do zapytań AJAX
 export const auctionService = {
     async getAll() {
-        return axiosInstance.get(BASE_ENDPOINT);
+        return axiosInstance.get<AuctionItem[]>(BASE_ENDPOINT);
     },
     // mapped Types in TS
     async getOne(id: AuctionItem['id']) {
        // console.log(id);
-        return axiosInstance.get(BASE_ENDPOINT+'/'+id);
+        return axiosInstance.get<AuctionItem>(BASE_ENDPOINT+'/'+id);
     },
     // utility Types in TS
     async addOne(auction: Omit<AuctionItem, 'id'>) {
         //console.log(auction);
-        return axiosInstance.post(BASE_ENDPOINT, auction);
+        return axiosInstance.post<AuctionItem>(BASE_ENDPOINT, auction);
     }
 }
